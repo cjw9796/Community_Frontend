3146d74e444ca5e60e216ddd573e5e9c989ea87e
JUNGWOOKCHOI <cjw9796@gmail.com>
2023-08-26 21:47:40 +0900

    first

---
+++ src/main/java/com/kh/myproject/community/plan/controller/PlanController.java
@@ -0,0 +1,36 @@
+package com.kh.myproject.community.plan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PlanController {
+
+    //일정 리스트(일정 메인)
+    @GetMapping("/community/plan") // http://localhost:8070/community/plan
+    public String communityplan() {
+
+          return "community/plan/plan";
+
+    }
+
+    //일정 글 정보
+    @GetMapping("/community/plan/detail") // http://localhost:8070/community/plan/detail
+    public String communityplandetail() {
+
+
+        return "community/plan/plan_detail";
+    }
+
+    //일정 글 쓰기
+    @GetMapping("/community/plan/write") // http://localhost:8070/community/plan/write
+    public String communityplanwrite() {
+
+
+        return "community/plan/plan_write";
+    }
+
+
+
+
+}
